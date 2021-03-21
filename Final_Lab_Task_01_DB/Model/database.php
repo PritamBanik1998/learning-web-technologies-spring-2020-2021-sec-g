87ca805759b1d43aebdec065ff05f16dfe509ea3
Pritam <pritambanik71432@gmail.com>
2021-03-21 12:08:43 +0600

    model folder commit

---
+++ Final_Lab_Task_01_DB/Model/database.php
@@ -0,0 +1,18 @@
+<?php
+
+	$dbname = "pharmacist";
+	$dbuser	= "root";
+	$dbpass	= "";
+	$host	= "localhost";
+
+
+	function getConnection(){
+
+		global $dbname, $dbuser, $dbpass, $host;
+
+		$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+		return $conn;
+	}
+
+
+?>
