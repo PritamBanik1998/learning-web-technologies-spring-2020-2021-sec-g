c019e15a93a3c332311a0a07c90f0f8cf3521968
Pritam <pritambanik71432@gmail.com>
2021-03-21 12:16:08 +0600

    admin dashboard commit

---
+++ Final_Lab_Task_01_DB/View/admindashboard.php
@@ -0,0 +1,34 @@
+<?php
+        require_once('../Model/userModel.php');
+
+        $users = getAllUser();
+
+       
+            echo "<table border=1>
+                    <tr>
+                        <td>ID</td>
+                        <td>Username</td>
+                        <td>Email</td>
+                        <td>Type</td>
+                        <td>Action</td>
+                    </tr>";
+
+        foreach($users as $user)
+        {
+            
+       
+             echo 	"<tr>
+                        <td>{$user['ID']}</td>
+                        <td>{$user['Name']}</td>
+                        <td>{$user['Email']}</td>
+                        <td>{$user['Role']}</td>
+                        <td><a href = view.php>View|</a><a href = edit.php>Edit|</a><a href = delete.php>Delete</a></td>
+                       
+                    </tr>";
+
+        }
+        
+             echo "</table>";
+
+
+?>
